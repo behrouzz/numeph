268e9a6aec82be54889fb1d82f52fbc246e4711e
Behrouz <behrouz.safari@gmail.com>
2022-03-05 10:49:48 +0100

    fix bug time slicing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="numeph",
-    version="0.3.1",
+    version="0.3.2",
     author="Behrouz Safari",
     author_email="behrouz.safari@gmail.com",
     description="Convert JPL SPK ephemeris to numpy array",
